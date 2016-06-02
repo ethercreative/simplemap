dc0a3ad0ca52c9f7ebca9ffc0fb7948b0ed6a7aa
John Ripmeester <john@bureauvandemeester.nl>
2016-06-02 09:51:55 +0200

    Updated simplemap.js
    Updated simplemap.js to fix bug when the marker is dropped the lat lng fields are filled with "return function a" due to js function returning a function and not the lat/lng value. This caused the google map to crash on reload and not show any maps.

--- resources/SimpleMap_Map.js
+++ resources/SimpleMap_Map.js
@@ -229,7 +229,7 @@ SimpleMap.prototype.sync = function (update) {
 		self.inputs.address.value = address;
 	});
 
-	if (update) return this.update(pos.lat, pos.lng, true);
+	if (update) return this.update(pos.lat(), pos.lng(), true);
 	return this;
 };
 

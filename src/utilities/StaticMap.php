b9346e3139ee8757d1245936e671a9c7d8d2420c
Tam <hi@tam.sx>
2019-08-29 10:00:29 +0100

    Add todo

--- src/utilities/StaticMap.php
+++ src/utilities/StaticMap.php
@@ -11,6 +11,8 @@ namespace ether\simplemap\utilities;
 /**
  * Class StaticMap
  *
+ * TODO: Build from https://github.com/dfacts/staticmaplite/blob/master/staticmap.php
+ *
  * @author  Ether Creative
  * @package ether\simplemap\utilities
  */
@@ -41,10 +43,10 @@ class StaticMap
 	public function render ()
 	{
 		header("Content-Type: image/png");
-		$im = @imagecreate(110, 20) or die("Cannot Initialize new GD image stream");
+		$im = @imagecreate($this->width, $this->height) or die("Cannot Initialize new GD image stream");
 		$background_color = imagecolorallocate($im, 0, 0, 0);
 		$text_color       = imagecolorallocate($im, 233, 14, 91);
-		imagestring($im, 1, 5, 5, "A Simple Text String", $text_color);
+		imagestring($im, 1, 5, 5, "TODO: Static map", $text_color);
 		imagepng($im);
 		imagedestroy($im);
 	}
